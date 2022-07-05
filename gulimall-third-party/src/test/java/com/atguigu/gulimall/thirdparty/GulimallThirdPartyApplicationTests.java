f86b9813caf6b1ce8fa88fe68b135496c62ce947
lbw <53471996+NIU-GX@users.noreply.github.com>
2022-07-05 17:23:04 +0800

    Update GulimallThirdPartyApplicationTests.java

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -24,8 +24,8 @@ class GulimallThirdPartyApplicationTests {
 //        // Endpoint以杭州为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-cn-beijing.aliyuncs.com";
 //        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//        String accessKeyId = "LTAI4GAo2yQ8N92xjnoXpyG3";
-//        String accessKeySecret = "aLeBlBP9ENKzP0JFRLIIUldODIWtQv";
+//        String accessKeyId = "";
+//        String accessKeySecret = "";
 //
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
